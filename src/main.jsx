a4b13221fae454a64e130ace7d135d9fcc6eee4c
Soubhagya Ranjan Rout <routsoubhagyar433@gmail.com>
2025-12-23 12:52:04 +0530

    add

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { StrictMode } from "react";
+import TodoApp from "./Redux/ReduxTodo.jsx";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store/store.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <TodoApp />
+    </Provider>
+  </StrictMode>
+);
